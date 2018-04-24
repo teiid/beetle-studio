f3ae342abdb363872c7d4783af9a1e26afd444b1
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2018-04-24 18:16:34 +0100

    Improvements to example odata virt client
    * Adds full url capability
    
    * Changes names of virt rather than vdb

--- openshift/examples/odata-virt/odatavirt.js
+++ openshift/examples/odata-virt/odatavirt.js
@@ -1,7 +1,9 @@
 const odata = require('odata-client');
 var args = require('optimist').argv,
-    help = 'An odata client for testing a usstates vdb.\n' +
-            '\t-u: base location url of the vdb service\n' +
+    help = 'An odata client for testing a usstates virtualization. ' +
+           'Either specify the full url (-l) or construct from virtualization components (-u, -v, -m, -d).\n' +
+            '\t-l: the full url\n' +
+            '\t-u: base location url of the virtualization service\n' +
             '\t-v: name of the vdb\n' +
             '\t-m: name of the vdb model\n' +
             '\t-d: name of the ddl view\n';
@@ -11,36 +13,43 @@ if ((args.h) || (args.help)) {
   process.exit(0);
 }
 
+var serviceUrl;
 if ((args.u)) {
   var url = args.u;
-} else {
-  console.error("No base location url specified... exiting");
-  process.exit(1);
-}
 
-if ((args.v)) {
-  var vdbName = args.v;
-} else {
-  console.error("No vdb name specified... exiting");
-  process.exit(1);
-}
+  if ((args.v)) {
+    var vdbName = args.v;
+  } else {
+    console.error("Error: No vdb name specified... exiting");
+    process.exit(1);
+  }
 
-if ((args.m)) {
-  var modelName = args.m;
-} else {
-  console.error("No vdb model specified... exiting");
-  process.exit(1);
-}
+  if ((args.m)) {
+    var modelName = args.m;
+  } else {
+    console.error("Error: No vdb model specified... exiting");
+    process.exit(1);
+  }
 
-if ((args.d)) {
-  var ddlView = args.d;
+  if ((args.d)) {
+    var ddlView = args.d;
+  } else {
+    console.error("Error: No ddl view specified... exiting");
+    process.exit(1);
+  }
+
+  serviceUrl = url + '/odata4/' + vdbName + '/' + modelName + '/' + ddlView;
+
+} else if ((args.l)) {
+  serviceUrl = args.l;
+
 } else {
-  console.error("No ddl view specified... exiting");
+  console.error("Error: No base location url (-u) or full url (-l) specified... exiting\n");
+  console.error(help);
   process.exit(1);
 }
 
 // http://usstatesdsvdb-odata-beetle-studio.birds-of-prey.phantomjinx.org.uk/odata4/usstatesdsvdb/views/statesView
-var serviceUrl = url + '/odata4/' + vdbName + '/' + modelName + '/' + ddlView;
 console.log("\nQuerying with odata url:\n" + serviceUrl + "\n\n===");
 
 var config = {
